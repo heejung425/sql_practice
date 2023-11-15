49389719ce1b71566960a9c9edaa99f80ac65c36
heejung425 <36969813+heejung425@users.noreply.github.com>
2023-11-15 14:44:17 -0800

    Update and rename basic_to_intermediate.md to basic_to_intermediate.sql

---
+++ HackerRank/basic_to_intermediate.sql
@@ -0,0 +1,56 @@
+
+-- 1. Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+
+
+SELECT
+  DISTINCT city 
+FROM station
+WHERE LEFT(city,1) NOT IN('a','e','i','o','u')
+  AND RIGHT(city,1) NOT IN('a','e','i','o','u')
+
+
+-- 2. Write a query that prints a list of employee names for employees in Employee having a salary greater than $2,000 per month who have been employees for less than 10 months. Sort your result by ascending employee_id.
+
+
+SELECT
+  name
+FROM employee
+WHERE salary > 2000
+  AND months < 10
+ORDER BY employee_id
+
+
+
+-- 3. Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345. Round your answer to 4 decimal places.
+   
+
+SELECT 
+  ROUND(long_w,4)
+FROM station
+WHERE lat_n =
+  (SELECT MAX(lat_n) FROM station WHERE lat_n < 137.2345)
+
+
+-- 4. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings.
+
+
+SELECT 
+    months * salary AS earnings,
+    COUNT(*)
+FROM employee
+GROUP BY earnings
+ORDER BY earnings DESC
+LIMIT 1
+
+
+-- 5. Write a query identifying the type of each record in the TRIANGLES table using its three side lengths.
+
+SELECT CASE
+            WHEN A = B AND B = C THEN 'Equilateral'
+            WHEN A + B <= C  OR B + C <= A OR A + C <= B THEN 'Not A Triangle'
+            WHEN A = B OR B = C OR C = A THEN 'Isosceles'
+            ELSE 'Scalene'
+        END AS 'shape'
+FROM TRIANGLES
+
+
