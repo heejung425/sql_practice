8afc3a77b18abe4223e21ece93fcbb363e0da83f
heejung425 <36969813+heejung425@users.noreply.github.com>
2023-11-15 15:08:21 -0800

    Update intermediate.sql

--- HackerRank/intermediate.sql
+++ HackerRank/intermediate.sql
@@ -17,3 +17,18 @@ FROM functions AS f1
     ON f1.x = f2.y AND f2.x = f1.y
 WHERE f1.x < f1.y
 ORDER BY x
+
+
+
+/* 2. Consider p1(a,b) and p2(c,d) to be two points on a 2D plane.
+a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points p1 and p2 and round it to a scale of 4 decimal places.
+*/
+
+SELECT  
+    ROUND((ABS(MIN(lat_n) - MAX(lat_n)) + ABS(MIN(long_w) - MAX(long_w))),4)
+    -- Manhattan Distance : p1 at (x1, y1) and p2 at (x2, y2), it is |x1 - x2| + |y1 - y2|
+FROM station
