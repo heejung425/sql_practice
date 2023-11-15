96fb6c51c69171eeb9a43d6bd2400832d2386b98
heejung425 <36969813+heejung425@users.noreply.github.com>
2023-11-15 14:42:57 -0800

    Update intermediate.sql

--- HackerRank/intermediate.sql
+++ HackerRank/intermediate.sql
@@ -1,4 +1,7 @@
--- 1. You are given a table, Functions, containing two columns: X and Y. Two pairs (X1, Y1) and (X2, Y2) are said to be symmetric pairs if X1 = Y2 and X2 = Y1. Write a query to output all such symmetric pairs in ascending order by the value of X. List the rows such that X1 ≤ Y1.
+/* 1. You are given a table, Functions, containing two columns: X and Y. 
+Two pairs (X1, Y1) and (X2, Y2) are said to be symmetric pairs if X1 = Y2 and X2 = Y1. 
+Write a query to output all such symmetric pairs in ascending order by the value of X. List the rows such that X1 ≤ Y1.
+*/
 
 SELECT x, y
 FROM functions
@@ -14,4 +17,3 @@ FROM functions AS f1
     ON f1.x = f2.y AND f2.x = f1.y
 WHERE f1.x < f1.y
 ORDER BY x
-
