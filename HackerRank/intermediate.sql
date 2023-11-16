94c0c32495d0d7253061669db7a7865e3604268e
heejung425 <36969813+heejung425@users.noreply.github.com>
2023-11-16 12:14:18 -0800

    Update intermediate.sql

--- HackerRank/intermediate.sql
+++ HackerRank/intermediate.sql
@@ -55,6 +55,8 @@ FROM company AS c
 GROUP BY c.company_code, c.founder
 ORDER BY c.company_code
 
+
+    
 /*4. Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! 
 Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. 
 Order your output in descending order by the total number of challenges in which the hacker earned a full score. 
