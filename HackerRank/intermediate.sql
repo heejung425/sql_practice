08c84f83feaf7d2938f6a550ebec6ee0bdfaa9b8
heejung425 <36969813+heejung425@users.noreply.github.com>
2023-11-16 10:17:27 -0800

    Update intermediate.sql

--- HackerRank/intermediate.sql
+++ HackerRank/intermediate.sql
@@ -31,3 +31,27 @@ Query the Manhattan Distance between points p1 and p2 and round it to a scale of
 SELECT  
     ROUND((ABS(MIN(lat_n) - MAX(lat_n)) + ABS(MIN(long_w) - MAX(long_w))),4) -- Manhattan Distance : p1 at (x1, y1) and p2 at (x2, y2), it is |x1 - x2| + |y1 - y2|
 FROM station
+
+
+    
+/*3. Amber's conglomerate corporation just acquired some new companies. Each of the companies follows this hierarchy: 
+Founder > Lead Manager > Senior Manager > Manager > Employee
+Write a query to print the company_code, founder name, total # of lead managers, total # of senior managers, total # of managers, and total # of employees. 
+Order your output by ascending company_code.
+*/
+    
+SELECT  
+    c.company_code, 
+    c.founder, 
+    COUNT(DISTINCT lm.lead_manager_code) AS total_lead_managers, 
+    COUNT(DISTINCT sm.senior_manager_code) AS total_senior_managers, 
+    COUNT(DISTINCT m.manager_code) AS total_managers, 
+    COUNT(DISTINCT e.employee_code) AS total_employees
+FROM company AS c
+    LEFT JOIN lead_manager AS lm ON lm.company_code = c.company_code
+    LEFT JOIN senior_manager AS sm ON sm.lead_manager_code = lm.lead_manager_code
+    LEFT JOIN manager AS m ON m.senior_manager_code = sm.senior_manager_code
+    LEFT JOIN employee AS e ON e.manager_code = m.manager_code
+GROUP BY c.company_code, c.founder
+ORDER BY c.company_code
+
